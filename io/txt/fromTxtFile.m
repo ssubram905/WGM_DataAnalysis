57f6837cd966843263c32f573fefd75bc1fd863f
ssubram905 <54635127+ssubram905@users.noreply.github.com>
2020-03-20 16:33:28 +0000

    Update to include input in both nm and fm

--- io/txt/fromTxtFile.m
+++ io/txt/fromTxtFile.m
@@ -33,9 +33,13 @@ time = fileinfo.data(sIndex:eIndex,1);
 eTime = time(end);
 plot1 = zeros(eIndex-sIndex+1,ndata);
 plot2 = zeros(eIndex-sIndex+1,ndata);
-for i = 1:ndata
+for i = 1:ndata   
     plot1(:,i) = (fileinfo.data(sIndex:eIndex,i+1) - min(fileinfo.data(sIndex:eIndex,i+1)));
     plot2(:,i) = fileinfo.data(sIndex:eIndex,i+ndata+1);
+    if ~contains(fileinfo.textdata(end),'[fm]')
+        plot1(:,i) = plot1(:,i)*1e6;
+        plot2(:,i) = plot2(:,i)*1e6;
+    end
 end
 
 results.sIndex = 1;
